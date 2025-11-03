b46b4e05ef4a71db0fef60ea1f3e2596a743fc71
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2025-11-03 16:18:52 +0530

    basic flask app

---
+++ Flask/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    day_of_week = datetime.today().strftime('%A')
+    return render_template("index.html", day_of_week=day_of_week)
+
+@app.route("/api/<name>")
+def dynamic_route(name):
+    result = "<p>Hello " +name+ "</p>"
+    return result
+
+@app.route("/query-params")
+def query_param():
+    name = request.values.get("user")
+    result = "<p>Hello " +name+ "</p>"
+    return result
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
